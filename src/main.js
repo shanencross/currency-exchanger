5b0bd92816477f487a4842f5d4c6a6d80f54877e
Shanen Cross <shanencross@yahoo.com>
2021-04-01 02:41:58 -0700

    Place API call in submit event listener for form

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './services/exchange-rate-service.js';
 
-console.log(process.env.API_KEY);
+$("form#currency-form").submit(function(event) {
+  event.preventDefault();
 
-let response = ExchangeRateService.fetchExchangeRateData();
-response.then(function(nextResponse) {
-  console.log(nextResponse);
+  let response = ExchangeRateService.fetchExchangeRateData();
+  response.then(function(nextResponse) {
+    console.log(nextResponse);
+  });
 });
 
