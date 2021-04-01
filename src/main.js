32a9ec929cc857621e4f11638a9be9224c3ad98f
Shanen Cross <shanencross@yahoo.com>
2021-04-01 01:34:56 -0700

    Test out API call with API .env key

--- src/main.js
+++ src/main.js
@@ -2,3 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+console.log(process.env.API_KEY);
+
+let response = fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  .then(function(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .catch(function(error) {
+    return error;
+  });
+
+response.then(function(nextResponse) {
+  console.log(nextResponse);
+});
+
