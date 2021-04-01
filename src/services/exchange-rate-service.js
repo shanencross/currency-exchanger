46f142833983c3869272580d45e4d0daac074d7a
Shanen Cross <shanencross@yahoo.com>
2021-04-01 14:57:08 -0700

    Change to pair conversion API call, taking initial currency, final currency, and value as input

--- src/services/exchange-rate-service.js
+++ src/services/exchange-rate-service.js
@@ -1,6 +1,6 @@
 export default class ExchangeRateService {
-  static fetchExchangeRateData() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  static fetchPairConversionData(initialCurrency, finalCurrency, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${initialCurrency}/${finalCurrency}/${amount}`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
