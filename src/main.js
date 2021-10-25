d54666176850cea42f976e7c2c28a152275d964d
Shanen Cross <shanencross@yahoo.com>
2021-10-25 14:46:00 -0700

    Remove console log from GitHub test

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ $("form#currency-form").submit(function(event) {
 
   const moneyUSD = $("input#USD-input").val();
   const newCurrency = $("input#currency-input").val().toUpperCase();
-  console.log("testing something with github");
 
   ExchangeRateService.fetchPairConversionData("USD", newCurrency, moneyUSD)
     .then(function(response) {
